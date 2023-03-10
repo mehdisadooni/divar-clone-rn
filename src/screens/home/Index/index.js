dd54385dccc801fef399daa7c5dd3b8ffa760936
Mehdi sadooni <mdi.mehdi97@gmail.com>
2023-03-10 20:37:55 +0330

    complete search container in home screen

--- src/screens/home/Index/index.js
+++ src/screens/home/Index/index.js
@@ -1,11 +1,32 @@
 import {View, Text} from "react-native";
 import React from "react";
+import {styles} from "./styles";
+import {SafeAreaView} from "react-native-safe-area-context";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import {colors} from "../../../utils/colors";
 
 const HomeIndex = () => {
     return (
-        <View>
-            <Text>HomeIndex Screen</Text>
-        </View>
+        <SafeAreaView>
+            <View style={styles.headerContainer}>
+                <View style={styles.searchContainer}>
+                    <View style={styles.placeholder}>
+                        <FontAwesome name='search' color={colors.gray} size={20}/>
+                        <Text style={styles.placeholderText}>جستجو در همه ی آگهی ها</Text>
+                    </View>
+                    <View style={styles.location}>
+                        <View style={styles.verticalLine}></View>
+                        <Text style={styles.locationText}>تهران</Text>
+                        <FontAwesome name='map-marker' color={colors.gray} size={20}/>
+                    </View>
+                </View>
+
+                <View style={styles.categoriesContainer}>
+
+                </View>
+
+            </View>
+        </SafeAreaView>
     )
 }
 
---
+++ src/screens/home/Index/styles.js
@@ -0,0 +1,49 @@
+import {StyleSheet} from "react-native";
+import {colors} from "../../../utils/colors";
+import {fonts} from "../../../utils/fonts";
+
+export const styles = StyleSheet.create({
+    headerContainer: {
+        backgroundColor: '#eae9e9',
+        height: 100
+    },
+    searchContainer: {
+        borderRadius:4,
+        borderColor:'#cccccc',
+        borderWidth:1,
+        backgroundColor: '#fff',
+        height: 40,
+        paddingHorizontal:10,
+        marginTop: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '97%',
+        alignSelf: 'center'
+    },
+    placeholder: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    placeholderText: {
+        color:colors.gray,
+        fontFamily:fonts.iranSans,
+        paddingRight:5,
+
+    },
+    locationText: {
+        fontFamily:fonts.iranSans,
+        marginRight:7
+    },
+    location: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    verticalLine: {
+        borderLeftWidth: 1,
+        borderLeftColor: '#cccccc',
+        height: '50%',
+        paddingLeft:15,
+        alignSelf: 'center',
+    },
+    categoriesContainer: {},
+})
